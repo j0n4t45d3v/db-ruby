8d4a6cb00293bf8a6f2eb18878195b21fdf6e1a3
Jonatas <delimajonatas875@gmail.com>
2025-09-24 11:58:00 -0300

    feat: core database

---
+++ src/core.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Core
+  def initialize(data:)
+    @data = data
+  end
+
+  # @param table [Model::Table] table base model
+  # @return [void]
+  def create_table(table:)
+    @data[table.name] = table.to_hash
+  end
+
+  # @param from [String] table name
+  # @param where [Hash] conditions
+  # @param fields [Array] fields to show in result. default [:all]
+  # @return [Array<Hash<String, Object>>]
+  def select(from:, where: {}, field: [:all])
+    data_filted = @data[from.to_sym][:data].select do |item|
+      where.all? { |key, condition| item[key] == condition[:value] }
+    end
+
+    field.include?(:all) ? data_filted : data_filted.map { |record| record.slice(*field) }
+  end
+
+  # @param from [String] table name
+  # @param data [Model::Record] data fields and values
+  # @return [void]
+  def insert(from:, data:)
+    @data[from] << data
+  end
+
+  def show_all
+    @data
+  end
+end
